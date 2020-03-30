bb36cbf6a066720d7f16b67be095a3263e38904b
Krishnan Govindraj <krishnangovindraj1794@gmail.com>
2020-03-31 01:39:00 +0200

    Rewrite the mai_experiment common code

--- mai_experiments/run_experiments_refactor/example_preprocessing_refactor.py
+++ mai_experiments/run_experiments_refactor/example_preprocessing_refactor.py
@@ -5,18 +5,19 @@ from problog.logic import Term
 from problog.program import SimpleProgram
 
 from mai_experiments.experiment_settings import FileNameData, DebugPrintingOptions
-from mai_version.IO.input_format import KnowledgeBaseFormat
-from mai_version.IO.label_collector import LabelCollectorMapper
-from mai_version.IO.parsing_background_knowledge import parse_background_knowledge_keys
-from mai_version.IO.parsing_examples import KeysExampleBuilder
-from mai_version.IO.parsing_settings.setting_parser import SettingsParserMapper
-from mai_version.IO.parsing_settings.utils import KeysPredictionGoalHandler
-from mai_version.representation.background_knowledge import BackgroundKnowledgeWrapper
-from mai_version.representation.example import InternalExampleFormat, ClauseDBExampleWrapper, Label, \
-    SimpleProgramExampleWrapper
-from mai_version.representation.example_collection import ExampleCollection
-from mai_version.representation.language import TypeModeLanguage
-from mai_version.trees.TreeBuilder import TreeBuilderType
+
+from refactor.tilde_essentials.tree import DecisionTree
+from refactor.tilde_essentials.tree_builder import TreeBuilder
+from refactor.query_testing_back_end.django.clause_handling import destruct_tree_tests
+from refactor.io.input_format import KnowledgeBaseFormat
+from refactor.io.label_collector import LabelCollectorMapper
+from refactor.io.parsing_background_knowledge import parse_background_knowledge_keys
+from refactor.io.parsing_examples import KeysExampleBuilder
+from refactor.io.parsing_settings.setting_parser import KeysSettingsParser, SettingsParserMapper
+from refactor.representation.example import InternalExampleFormat, ClauseDBExampleWrapper
+from refactor.model_factory import ModelFactory
+
+from mai_experiments.experiment_settings import FileNameData, DebugPrintingOptions
 
 
 class Experiment:
@@ -24,44 +25,44 @@ class Experiment:
     NOTE: we store the examples in 2 ways:
     * 'usable for training': the examples containing the classification predicate
     * 'usable for testing': the examples with the classification predicate removed
-
-
     """
 
-    def __init__(self):
+    def __init__(self, file_name_data: FileNameData):
+        self.file_name_data = file_name_data
+
         self.training_examples_collection = None
         self.examples_usable_for_testing = None
         self.possible_labels = None
 
         self.language = None
-        self.prediction_goal = None
+        self.prediction_goal_handler = None
+
+        self.model_factory = None
+
+    def parse_settings(self):
+        settings_file_parser = SettingsParserMapper.get_settings_parser(KnowledgeBaseFormat.KEYS)
+        parsed_settings = settings_file_parser.parse(self.file_name_data.fname_settings)
+
+        self.language = parsed_settings.language  # type: TypeModeLanguage
+        self.prediction_goal_handler = parsed_settings.get_prediction_goal_handler()  # type: KeysPredictionGoalHandler
+        self.prediction_goal = self.prediction_goal_handler.get_prediction_goal()  # type: Term
 
     def preprocess_examples_and_background_knowledge(self,
-                                                     file_name_data: FileNameData,
+                                                     model_factory: ModelFactory,
                                                      filter_out_unlabeled_examples: bool,
                                                      debug_printing_options: DebugPrintingOptions):
         engine = DefaultEngine()
         engine.unknown = 1
 
-        settings_file_parser = SettingsParserMapper.get_settings_parser(KnowledgeBaseFormat.KEYS)
-        parsed_settings = settings_file_parser.parse(file_name_data.fname_settings)
-
-        self.language = parsed_settings.language  # type: TypeModeLanguage
-
         kb_format = KnowledgeBaseFormat.KEYS
         internal_ex_format = InternalExampleFormat.CLAUSEDB
-
-        treebuilder_type = TreeBuilderType.DETERMINISTIC
-
-        prediction_goal_handler = parsed_settings.get_prediction_goal_handler()  # type: KeysPredictionGoalHandler
-        self.prediction_goal = prediction_goal_handler.get_prediction_goal()  # type: Term
 
         # ------------------------------------------------
         # --- BACKGROUND KNOWLEDGE -----------------------
         # ------------------------------------------------
 
         background_knowledge_wrapper \
-            = parse_background_knowledge_keys(file_name_data.fname_background,
+            = parse_background_knowledge_keys(self.file_name_data.fname_background,
                                               self.prediction_goal)  # type: BackgroundKnowledgeWrapper
 
         full_background_knowledge_sp \
@@ -71,12 +72,12 @@ class Experiment:
 
         # EXAMPLES
         example_builder = KeysExampleBuilder(self.prediction_goal, debug_printing_options.example_parsing)
-        self.training_examples_collection = example_builder.parse(internal_ex_format, file_name_data.fname_examples,
+        self.training_examples_collection = example_builder.parse(internal_ex_format, self.file_name_data.fname_examples,
                                                                   full_background_knowledge_sp)  # type: ExampleCollection
 
         # ------------------------------------------------
         # --- LABELS -------------------------------------
-        index_of_label_var = prediction_goal_handler.get_predicate_goal_index_of_label_var()  # type: int
+        index_of_label_var =  self.prediction_goal_handler.get_predicate_goal_index_of_label_var()  # type: int
         label_collector = LabelCollectorMapper.get_label_collector(internal_ex_format, self.prediction_goal,
                                                                    index_of_label_var,
                                                                    engine=engine)
@@ -97,7 +98,16 @@ class Experiment:
 
         # ------------------------------------------------
 
+
+        # Saturate the examples.
+        print("Start saturating examples")
+        rule_grounder = model_factory.get_rule_grounder(full_background_knowledge_sp, self.language, self.prediction_goal_handler)
+        rule_grounder.setup()
+        rule_grounder.saturate_examples(self.training_examples_collection.get_example_wrappers_sp())
+        print("Finish saturating examples")
+
         stripped_examples_simple_program = self.training_examples_collection.get_labeled_example_wrappers_sp()  # type: List[SimpleProgramExampleWrapper]
+
         self.examples_usable_for_testing = stripped_examples_simple_program  # type: List[SimpleProgramExampleWrapper]
 
         if internal_ex_format == InternalExampleFormat.CLAUSEDB:
--- mai_experiments/run_experiments_refactor/refactor_experiment_template.py
+++ mai_experiments/run_experiments_refactor/refactor_experiment_template.py
@@ -9,7 +9,8 @@ from mai_experiments.fold_control import FoldInfo
 from mai_experiments.fold_control import FoldInfoController
 from mai_experiments.fold_example_splitting import FoldExampleSplitter
 from mai_experiments.run_experiments_refactor.example_preprocessing_refactor import Experiment
-from refactor.default_interface import DefaultHandler
+from refactor.model_factory import ModelFactory
+from refactor.tilde_config import TildeConfig
 from refactor.tilde_essentials.tree import DecisionTree, write_out_tree
 from refactor.tilde_essentials.tree_builder import TreeBuilder
 from refactor.tilde_essentials.tree_pruning import prune_leaf_nodes_with_same_label
@@ -18,11 +19,12 @@ from mai_version.representation.example import ExampleWrapper
 from mai_version.representation.example_collection import ExampleCollection
 
 
-def run_experiment(file_name_data: FileNameData, fold_info_controller: FoldInfoController,
-                   default_handler: DefaultHandler,
+def run_experiment(tilde_config: TildeConfig, backend_choice: ModelFactory.BackendChoice,
+                   file_name_data: FileNameData, fold_info_controller: FoldInfoController,
                    hide_printouts: bool = False,
                    filter_out_unlabeled_examples=False,
-                   debug_printing_options=DebugPrintingOptions()):
+                   debug_printing_options=DebugPrintingOptions(),
+                   random_forest_options: ModelFactory.RandomForestOptions=None):
     # -- create output directory
     if not os.path.exists(file_name_data.output_dir):
         os.makedirs(file_name_data.output_dir)
@@ -32,26 +34,31 @@ def run_experiment(file_name_data: FileNameData, fold_info_controller: FoldInfoC
     if hide_printouts:
         sys.stdout = open(os.devnull, "w")
 
-    experiment = Experiment()
-    experiment.preprocess_examples_and_background_knowledge(file_name_data, filter_out_unlabeled_examples,
-                                                            debug_printing_options)
-
+    experiment = Experiment(file_name_data)
+    experiment.parse_settings()
+    model_factory = ModelFactory(tilde_config, experiment.language, backend_choice)
+    experiment.preprocess_examples_and_background_knowledge(model_factory, filter_out_unlabeled_examples, debug_printing_options)
+    
     fold_example_splitter = FoldExampleSplitter(fold_info_controller)
     for fold_info, training_examples_collection, test_examples in fold_example_splitter.fold_split_generator(
             experiment):  # type: FoldInfo, ExampleCollection, List[ExampleWrapper]
         print("fold: ", fold_info.index)
-        training_examples = default_handler.get_transformed_example_list(training_examples_collection)
+        
+        tree_builder = model_factory.get_default_decision_tree_builder()  # type: TreeBuilder
+                                                                 
+        training_examples = tree_builder.splitter.test_evaluator.get_transformed_example_list(training_examples_collection.get_example_wrappers_sp())
 
-        tree_builder = default_handler.get_default_decision_tree_builder(experiment.language,
-                                                                         experiment.prediction_goal)  # type: TreeBuilder
-        decision_tree = DecisionTree()
+        if random_forest_options is None:
+            model = DecisionTree()
+        else:
+            model = model_factory._create_random_forest(tree_builder, random_forest_options)    
         start_build_time = time.time()
-        decision_tree.fit(examples=training_examples, tree_builder=tree_builder)
+        model.fit(examples=training_examples, tree_builder=tree_builder)
 
         if debug_printing_options.tree_building:
             print("unpruned:")
-            print(decision_tree)
-        decision_tree.prune(prune_leaf_nodes_with_same_label)
+            print(model)
+        model.prune(prune_leaf_nodes_with_same_label)
         end_build_time = time.time()
 
         # run_time_sec = end_time - start_time
@@ -61,16 +68,16 @@ def run_experiment(file_name_data: FileNameData, fold_info_controller: FoldInfoC
         if debug_printing_options.tree_building or debug_printing_options.tree_pruning:
             print("build time (ms):", build_time_ms)
             print("pruned")
-            print(decision_tree)
+            print(model)
 
         # write out tree
         tree_fname = os.path.join(file_name_data.output_dir,
-                                  default_handler.back_end_name + "_" + fold_info_controller.fname_prefix_fold + '_fold' + str(
+                                  model_factory.backend_choice.name + "_" + fold_info_controller.fname_prefix_fold + '_fold' + str(
                                       fold_info.index) + ".tree")
-        write_out_tree(tree_fname, decision_tree)
+        write_out_tree(tree_fname, model)
 
         start_test_example_transformation = time.time()
-        test_examples_reformed = default_handler.get_transformed_test_example_list(test_examples)
+        test_examples_reformed = tree_builder.splitter.test_evaluator.get_transformed_example_list(test_examples)
         # for ex_wr_sp in test_examples:
         #     example_clause = build_clause(ex_wr_sp, training=False)
         #     example = Example(data=example_clause, label=ex_wr_sp.label)
@@ -78,7 +85,7 @@ def run_experiment(file_name_data: FileNameData, fold_info_controller: FoldInfoC
         #     test_examples_reformed.append(example)
         end_test_example_transformation = time.time()
 
-        statistics_handler = verify(decision_tree, test_examples_reformed)
+        statistics_handler = verify(model, test_examples_reformed)
         accuracy = statistics_handler.get_accuracy()
         if debug_printing_options.get_classifier:
             print("accuracy:", accuracy)
@@ -94,7 +101,7 @@ def run_experiment(file_name_data: FileNameData, fold_info_controller: FoldInfoC
         fold_info.accuracy = accuracy
 
         statistics_fname = os.path.join(file_name_data.output_dir,
-                                        default_handler.back_end_name + "_" + fold_info_controller.fname_prefix_fold + '_fold'
+                                        model_factory.backend_choice.name + "_" + fold_info_controller.fname_prefix_fold + '_fold'
                                         + str(fold_info.index) + ".statistics")
 
         # statistics_fname = file_name_data.output_dir + .fname_prefix_fold + '_fold' + str(fold_index) + ".statistics"
@@ -103,10 +110,13 @@ def run_experiment(file_name_data: FileNameData, fold_info_controller: FoldInfoC
         with open(statistics_fname, 'a') as f:
             f.write('\n\nnb of TRAINING ex: ' + str(len(training_examples)) + "\n")
             f.write('nb of TEST ex: ' + str(len(test_examples)) + "\n\n")
-            nb_nodes = decision_tree.get_nb_of_nodes()
+            if random_forest_options is not None:
+                f.write('nb of trees: ' + str(model.n_trees))
+                
+            nb_nodes = model.get_nb_of_nodes()
             fold_info.n_nodes = nb_nodes
             f.write("total nb of nodes: " + str(nb_nodes) + "\n")
-            nb_inner_nodes = decision_tree.get_nb_of_inner_nodes()
+            nb_inner_nodes = model.get_nb_of_inner_nodes()
             fold_info.n_inner_nodes = nb_inner_nodes
             f.write("nb of internal nodes: " + str(nb_inner_nodes) + "\n\n")
             f.write("execution time of fold (ms): " + str(elapsed_time_ms) + "\n")
@@ -116,7 +126,7 @@ def run_experiment(file_name_data: FileNameData, fold_info_controller: FoldInfoC
         # ------------------------------------------
 
         # --- DESTRUCTION (necessary for Django) ---
-        decision_tree.destruct()
+        model.destruct()
 
         for ex in training_examples:
             ex.destruct()
@@ -148,7 +158,7 @@ def run_experiment(file_name_data: FileNameData, fold_info_controller: FoldInfoC
         print("total time cross  (sum folds): " + str(total_execution_time_ms_of_cross_validation) + "\n")
 
     statistics_fname = os.path.join(file_name_data.output_dir,
-                                    default_handler.back_end_name + "_" + fold_info_controller.fname_prefix_fold + ".statistics")
+                                    model_factory.backend_choice.name + "_" + fold_info_controller.fname_prefix_fold + ".statistics")
     # statistics_fname = fd.dir_output_files + fd.fname_prefix_fold + ".statistics"
     with open(statistics_fname, 'w') as f:
         f.write("mean accuracy: " + str(mean_accuracy_of_folds) + "\n")
