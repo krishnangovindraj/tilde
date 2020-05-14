9f070c2e8ed89569a0d6ee8081a45ed5b9c5b797
Krishnan Govindraj <krishnangovindraj1794@gmail.com>
2020-05-15 01:19:43 +0200

    Add unify_vars, diff_vars special tests

---
+++ refactor/special_tests/unify_variables.py
@@ -0,0 +1,106 @@
+from typing import List, Tuple
+# from refactor.tilde_essentials.evaluation import TestEvaluator
+from refactor.tilde_essentials.example import Example
+from refactor.representation.TILDE_query import TILDEQuery
+from refactor.tilde_essentials.split_criterion import SplitCriterion
+
+from problog.program import SimpleProgram
+from problog.logic import Term, Constant, Not, Clause
+
+from refactor.logic_manipulation_utils import TermManipulationUtils, PartialSubstitutionDict
+from .special_test import SpecialTest, TildeTestResult
+""" A really simple 'auto' mode for constants """
+class UnifyVariablesTest(SpecialTest):
+
+    TEST_FUNCTOR_PREFIX = "tilde__unifyvars_"
+    ARG_MODES = ('+', '+')
+
+    def __init__(self, test_functor, type_name):
+        self.constant_type_name = type_name
+        super().__init__(test_functor, 2, self.ARG_MODES, (type_name, self.constant_type_name), [])
+        self.test_functor = test_functor
+        self.type_name = type_name
+
+        self.all_values = set()
+
+    """
+     Returns a TildeTestResult object
+    """
+    def run(self, placeholder_tilde_query: TILDEQuery, examples: List[Example], test_evaluator: 'TestEvaluator', split_criterion: SplitCriterion) \
+        -> TildeTestResult:
+        return TildeTestResult(placeholder_tilde_query, [])
+
+    """
+     Called once before the tree building begins so that any required pre-processing may be performed.
+    """
+    def setup(self, prediction_goal_handler, language, examples, bg_sp):
+        # we need to get all the possible values of the real_type
+        value_locations = {}
+        for functor, arg_modes in language.list_refinement_modes():
+            locations = []
+            arity = len(arg_modes)
+            arg_types = language.get_argument_types(functor, arity)
+            for i in range(len(arg_modes)):
+                if arg_types[i] == self.type_name: # and arg_modes[i] == '-':
+                    locations.append(i)
+
+            if len(locations) > 0:
+                value_locations[(functor, arity)] = locations
+
+        locations = []
+        for i in range(len(prediction_goal_handler.modes)):
+            if prediction_goal_handler.types[i] == self.type_name: # and prediction_goal_handler.modes[i] == '+': 
+                locations.append(i)
+
+        if len(locations) > 0:
+            value_locations[(prediction_goal_handler.functor, len(prediction_goal_handler.modes))] = locations
+
+        example_values = {}
+        for e in examples:
+            example_values[e] = []
+            for d in list(e.data) + [e.classification_term]:
+                if (d.functor, d.arity) in value_locations:
+                    for i in value_locations[(d.functor, d.arity)]:
+                        example_values[e].append(d.args[i])
+                        self.all_values.add( d.args[i] )
+
+        bg_values = set()
+        for b in bg_sp:
+            if type(b) == Clause and (b.head.functor, b.head.arity) in value_locations:
+                d = b.head
+            elif type(b) == Term and (b.functor, b.arity) in value_locations:
+                d = b
+            else:
+                d = None
+            if d is not None:
+                # This code is pointless if we are saturating examples.
+                for i in value_locations[(d.functor, d.arity)]:
+                    if isinstance(d.args[i], Constant):
+                        bg_values.add(d.args[i])
+                        self.all_values.add( d.args[i] )
+        
+        bg_values = list(bg_values)
+        # All work done here:
+        self._saturate_examples(examples, example_values, bg_values)
+
+    """
+     If the test is stable, we can chain it with later ones.
+     Else, an exception is thrown if chaining is attempted.
+    """
+    def is_stable(self):
+        return True
+
+    def _saturate_examples(self, examples: List[Example], example_values, bg_values):
+        for e in examples:
+            visible_values = self.all_values # example_values[e] + bg_values
+            new_facts = [Term(self.test_functor, g, g) for g in visible_values]
+            if self._needs_presaturation(e):
+                with e.extension_context() as ec:
+                    ec.extend(new_facts)
+            else:
+                e.add_facts(new_facts)
+
+
+    def _needs_presaturation(self, example: Example):
+        from refactor.query_testing_back_end.django.django_example import DjangoExample
+        return isinstance(example, DjangoExample)
